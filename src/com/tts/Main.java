465be4476e1a148705b67767bfb88f3ca63ff88b
sunu09 <74618256+sunu09@users.noreply.github.com>
2021-01-22 22:23:59 -0600

    2ND COMMIT-FOR LOOP

--- src/com/tts/Main.java
+++ src/com/tts/Main.java
@@ -12,23 +12,21 @@ public class Main {
         System.out.println("Guess a lucky number between 1 - 100");
         int num = input.nextInt();
 
-            if (num > 0 && num < 11) {
-                System.out.println("wow");
-            } else if (num > 5 && num < 16) {
-                System.out.println("so close");
-            } else {
-                System.out.println("nope! Try again.");
-            }
+            if (num > 10 && num < 16) {
+                System.out.println("Wow! your guess is correct.");
+                System.exit(0);
+                 } else
 
-        // score
-        System.out.println("What is your score?");
-        int score = input.nextInt();
-        int baseScore = 60;
-        if (score >baseScore) {
-            System.out.println("You have passed.");
-        } else {
-            System.out.println("YOu have failed.");
+                    {
+                    for (num = 0; num < 4; num++) {
+
+                    System.out.println("nope! Try again.");
+                    int num1 = input.nextInt();
+
+                        }
+                    }
+               
+               System.out.println("You reached the limitation. Try it later again.");
+           }
         }
 
-    }
-}
